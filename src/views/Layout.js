ea07adfb37f97e910b9427b9afda5edb13efffaa
Cihan Erman <cihanerman@Cihan-MacBook-Pro.local>
2018-11-03 22:42:57 +0300

    data bind

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -1,10 +1,17 @@
 var m = require("mithril")
 
 module.exports = {
+    text:'',
     view: function(vnode) {
         return m("main.layout", [
             m("nav.menu", [
-                m("a[href='/list']", {oncreate: m.route.link}, "Users")
+                m("a[href='/list']", {oncreate: m.route.link}, "Users"),
+                m('br'),
+                m('input[type=text]', {
+                    oninput: m.withAttr("value", function(value) {vnode.state.text = value})
+                }),
+                m('label','Search User: '),
+                m('span',vnode.state.text)
             ]),
             m("section", vnode.children)
         ])
